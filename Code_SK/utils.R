4afee69cb3711753760a816549c84dc3611eee6d
Sevvandi Kandanaarachchi <sevvandi.kandanaarachchi@monash.edu>
2022-01-20 12:41:33 +1100

    update utils

--- Code_SK/utils.R
+++ Code_SK/utils.R
@@ -10,3 +10,25 @@ average_over_epsilon <- function(x){
   }
   return(y)
 }
+
+
+normalize_for_AUC <- function(x, opt = 1){
+  # x has table to be normalized including cardinality
+  # opt = 1 -> divide by maximum
+  # opt = 2 -> divide by the mean
+  # opt = 3 or other number, do not divide
+  maxyval <- max(x[ ,-1], na.rm = TRUE)
+  meanyval <- mean(as.matrix(x[ ,-1]), na.rm = TRUE)
+  maxxval <- dim(x)[1]
+  if(opt == 1){
+    sc <- maxyval
+  }else if(opt == 2){
+    sc <- meanyval
+  }else{
+    sc <- 1
+  }
+  xout <- x
+  xout[ ,-1] <- x[ ,-1]/sc
+  xout[ ,1] <- x[ ,1]/maxxval
+  xout
+}
